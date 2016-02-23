10787418b0908c36c44127f873e665ba72d60f87
Funjando <andojuuichi@gmail.com>
2016-02-23 14:11:23 -0500

    Updated from Brython Server: 2/23/2016 2:11:33 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -24,9 +24,9 @@ Height of multiplication table: 8
 #Variables
 t_width=int(input("Width of multiplication table?"))
 t_height=int(input("Height of multiplication table?"))
-t_height=t_heightt
+t_heightt=t_height
 
 #Code
 while t_height>0:
-    print([x*[y for y in range(1, t_heightt+1)] for x in range(1, t_width+1)])
+    print([x*([y for y in range(1, t_heightt+1)]) for x in range(1, t_width+1)])
     t_height=t_height-1
